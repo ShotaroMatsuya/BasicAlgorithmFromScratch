2cfaa0f4e28d607649a00bc821a87d1a681b35c3
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-01-02 05:05:11 +0900

    FIX: small fix

--- MultiplePointer/main.js
+++ MultiplePointer/main.js
@@ -17,10 +17,10 @@ function countUniqueValues(arr) {
   return result;
 }
 
-// countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
-// countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
-// countUniqueValues([]); // 0
-// countUniqueValues([-2, -1, -1, 0, 1]); // 4
+countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
+countUniqueValues([]); // 0
+countUniqueValues([-2, -1, -1, 0, 1]); // 4
 
 function averagePair(arr, target) {
   let result = false;
@@ -46,10 +46,10 @@ function averagePair(arr, target) {
   return result;
 }
 
-// averagePair([1, 2, 3], 2.5); // true
-// averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); // true
-// averagePair([-1, 0, 3, 4, 5, 6], 4.1); // false
-// averagePair([], 4); // false
+averagePair([1, 2, 3], 2.5); // true
+averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); // true
+averagePair([-1, 0, 3, 4, 5, 6], 4.1); // false
+averagePair([], 4); // false
 
 function isSubsequence(str1, str2) {
   let i = 0;
