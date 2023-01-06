cf527d61e8b45db7923af9205baec60a8b2639fd
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-01-07 02:46:46 +0900

    ADD: singly linked list push

---
+++ SinglyLinkedList/main.js
@@ -0,0 +1,30 @@
+// piece of data - val
+// reference to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (this.head === null) this.head = newNode;
+    if (this.tail !== null) this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push('HI');
+list.push('You');
+console.log(list);
