344636eaddf51e39a1bdd43882b3acf2e7679d15
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-01-08 00:03:01 +0900

    ADD: push and pop solution

--- DoublyLinkedLists/main.js
+++ DoublyLinkedLists/main.js
@@ -12,4 +12,62 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+      return this;
+    }
+    let prev = this.tail;
+    newNode.prev = prev;
+    this.tail = newNode;
+    this.tail.prev.next = this.tail;
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (this.length === 0) return undefined;
+    let lastNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = lastNode.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+    return lastNode;
+  }
 }
+
+var doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.push(5); // doublyLinkedList
+doublyLinkedList.length; // 1
+doublyLinkedList.head.val; // 5
+doublyLinkedList.tail.val; // 5
+doublyLinkedList.head.prev; // null
+doublyLinkedList.push(10);
+doublyLinkedList;
+doublyLinkedList.length; // 2
+doublyLinkedList.head.val; // 5
+doublyLinkedList.head.next.val; // 10
+doublyLinkedList.tail.val; // 10
+doublyLinkedList.head.next.prev.val; // 10
+doublyLinkedList.push(15);
+doublyLinkedList;
+doublyLinkedList.length; // 3
+doublyLinkedList.head.val; // 5
+doublyLinkedList.tail.val; // 15
+doublyLinkedList.tail.prev.val; // 10
+doublyLinkedList.head.next.next.val; // 15
+
+doublyLinkedList.pop().val; // 15
+doublyLinkedList.length; // 2
+doublyLinkedList.pop().val; // 10
+doublyLinkedList.length; // 1
+doublyLinkedList.pop().val; // 5
+doublyLinkedList.length; // 0
+doublyLinkedList.pop(); // undefined
+doublyLinkedList.length; // 0
