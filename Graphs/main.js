0e9a6b0aa22cb703c0c336a01f3807e7d782bbed
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-01-14 13:50:10 +0900

    ADD: create a skelton of graph traversal

--- Graphs/main.js
+++ Graphs/main.js
@@ -38,3 +38,47 @@ g.addEdge('Hong Kong', 'Tokyo');
 g.addEdge('Hong Kong', 'Dallas');
 g.addEdge('Los Angels', 'Hong Kong');
 g.addEdge('Los Angels', 'Aspen');
+
+class GraphTraversal {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      v => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      v => v !== vertex1
+    );
+  }
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
+
+let gt = new GraphTraversal();
+gt.addVertex('A');
+gt.addVertex('B');
+gt.addVertex('C');
+gt.addVertex('D');
+gt.addVertex('E');
+gt.addVertex('F');
+
+gt.addEdge('A', 'B');
+gt.addEdge('A', 'C');
+gt.addEdge('B', 'D');
+gt.addEdge('C', 'E');
+gt.addEdge('D', 'E');
+gt.addEdge('D', 'F');
+gt.addEdge('E', 'F');
