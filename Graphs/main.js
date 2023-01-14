a1c0426c15f7f27fb8e041513bc9d7b9709eb19b
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-01-14 14:56:36 +0900

    ADD: bread first search

--- Graphs/main.js
+++ Graphs/main.js
@@ -107,6 +107,27 @@ class GraphTraversal {
     }
     return result;
   }
+
+  breadFirst(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 let gt = new GraphTraversal();
