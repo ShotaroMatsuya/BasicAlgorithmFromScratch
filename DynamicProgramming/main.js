88315bbea1168ff4b1fee04207cc2286b429c611
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-01-15 15:24:25 +0900

    ADD: Memorization solution

--- DynamicProgramming/main.js
+++ DynamicProgramming/main.js
@@ -5,3 +5,18 @@ function fib(n) {
   return fib(n - 2) + fib(n - 1);
 }
 
+// Memorization
+function fib2(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  let res = fib2(n - 1, memo) + fib2(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
+
+function fib3(n, memo = [undefined, 1, 1]) {
+  if (memo[n] !== undefined) return memo[n];
+  let res = fib2(n - 1, memo) + fib2(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
